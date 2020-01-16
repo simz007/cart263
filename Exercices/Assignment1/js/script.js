af312c7eb67672dbd0a0c38bcd41ac90f49ba45b
simz007 <51645428+simz007@users.noreply.github.com>
2020-01-16 17:22:35 -0500

    1: changed the color of the painted pixel from white to a random value

--- Exercices/Assignment1/js/script.js
+++ Exercices/Assignment1/js/script.js
@@ -13,7 +13,7 @@ A small DOM-based program for "painting" on div-based pixels.
 const NUM_PIXELS = 1000;
 const PIXEL_REVERT_DELAY = 1000;
 const DEFAULT_COLOR = 'black';
-const PAINT_COLOR = 'white';
+// const PAINT_COLOR = 'white';
 
 // Set up our starting function for when the page loads
 window.onload = setup;
@@ -42,10 +42,16 @@ function setup() {
 // the pixel's color and sets a timer for it to revert
 function paint(e) {
   // e.target contains the specific element moused over so let's
+
+
   // save that into a variable for clarity.
   let pixel = e.target;
-  // Change the background color of the element to white
-  pixel.style.backgroundColor = PAINT_COLOR;
+  // Give r g b random values
+  let r = Math.random() * 255;
+  let g = Math.random() * 255;
+  let b = Math.random() * 255;
+  // Change the background color of the element to the random values of r g b 
+  pixel.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
   // Set a timeout to call the reset function after a delay
   // When we pass additional parameters (like 'pixel' below) they
   // are passed to the callback function (resetPixel)
