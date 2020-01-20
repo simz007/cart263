db9dadffd408ab22d7160f3e65af05c66fd478ab
simz007 <51645428+simz007@users.noreply.github.com>
2020-01-20 10:59:05 -0500

    A2:  Added the click event listener
    added the click handler that calls the funtion (remove), wich turn the opacity of the pixel to 0

--- Exercices/Assignment1/js/script.js
+++ Exercices/Assignment1/js/script.js
@@ -31,6 +31,8 @@ function setup() {
     pixel.setAttribute('class', 'pixel');
     // Add a mouseover handler to the new element
     pixel.addEventListener('mouseover', paint);
+    // add a click handler to the new element that calls the remove function
+    pixel.addEventListener('click', remove);
     // Add the element to the body of the page
     document.body.appendChild(pixel);
   }
@@ -42,15 +44,13 @@ function setup() {
 // the pixel's color and sets a timer for it to revert
 function paint(e) {
   // e.target contains the specific element moused over so let's
-
-
   // save that into a variable for clarity.
   let pixel = e.target;
   // Give r g b random values
   let r = Math.random() * 255;
   let g = Math.random() * 255;
   let b = Math.random() * 255;
-  // Change the background color of the element to the random values of r g b 
+  // Change the background color of the element to the random values of r g b
   pixel.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
   // Set a timeout to call the reset function after a delay
   // When we pass additional parameters (like 'pixel' below) they
@@ -63,4 +63,14 @@ function paint(e) {
 // Takes the provided pixel element and sets its color back to default
 function resetPixel(pixel) {
   pixel.style.backgroundColor = DEFAULT_COLOR;
+}
+
+// remove function
+// removes a pixel from the screen (by setting opacity to 0)
+function remove(e) {
+  // e.target contains the specific element clicked
+  let pixel = e.target;
+  //set the pixel opacity to 0
+  pixel.style.opacity = '0';
+
 }
