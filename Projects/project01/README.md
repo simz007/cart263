6429d88696e6197c9a014493eb888af0f0c8591d
simz007 <51645428+simz007@users.noreply.github.com>
2020-01-31 10:20:04 -0500

    P1: Started Project 1
    - Got all the files and Jquery library links
    - Html layout of my first Start Screen
    -Css , styling the html with classes and IDs

---
+++ Projects/project01/README.md
@@ -0,0 +1,3 @@
+Fuzzy Puzzle
+Simon zogheib
+
---
+++ Projects/project01/js/script.js
@@ -0,0 +1,23 @@
+
+/*****************
+Fuzzy puzzle
+Simon Zogheib
+
+A puzzle game where you have to drag and dropthe
+4 pieces of the puzzle in place, but when you think you are done
+all the pieces will fall back out of place
+******************/
+
+
+
+//sound variables
+
+
+
+$(document).ready(function(){
+
+
+
+
+
+});
