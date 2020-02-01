0b5e7cbe6f0dfc82422f62877a942ff612300449
simz007 <51645428+simz007@users.noreply.github.com>
2020-02-01 13:40:12 -0500

    P1 : Added the droppable jquery interaction
    - Added the drop class and numbers to all the droppable divs of the right side of game sreen
    - Added the droppable interaction in the script and used a function to detect if the numbers match so the draggable can get dropped in the appropriate droppable.
    once its dropped i disabled the draggable option and  put the draggable in the center of the droppable with dragged.position

--- Projects/project01/js/script.js
+++ Projects/project01/js/script.js
@@ -4,7 +4,9 @@ Simon Zogheib
 
 A puzzle game where you have to drag and dropthe
 4 pieces of the puzzle in place, but when you think you are done
-all the pieces will fall back out of place
+all the pieces will fall back out of place.
+
+This is inspired by a previous student project (Crapy NAils) by Dana Riachi
 ******************/
 
 
@@ -33,6 +35,7 @@ $(document).ready(function() {
 
 
   //Setting up draggable elements
+
   $(".drag").draggable({
     //contained in window
     containment: "window",
@@ -43,13 +46,41 @@ $(document).ready(function() {
       ui.helper.data('dropped', false);
     },
 
-
   });
+  //End of draggable elements
 
 
 
+  //Setting up droppable elements
 
+  $(".drop").droppable({
+    //accept the draggables with same num attribute
+    accept: function(item) {
+      return $(this).attr('num') === item.attr('num');
+    },
+    //called when the appropriate draggable has been received
+    drop: function(event, ui) {
+      //notify the ui helper that the appropriate function is performed
+      // and that the draggable was dropped in the correct droppable
+      ui.helper.data('dropped', true);
 
+      
+
+      //store dropped draggable in a variable
+      let dragged = ui.draggable;
+
+      //disable draggable so u cant drag it after it's dropped in appropriate droppable
+      dragged.draggable("disable");
+
+      //put the draggable in the center of droppable
+      dragged.position({
+        my: "center",
+        at: "center",
+        of: $(this)
+      });
+    }
+  });
+  //End of droppable elements
 
 
 });
