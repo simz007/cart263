88904198c4d61ef64b8f383ee43491e2c9d29050
simz007 <51645428+simz007@users.noreply.github.com>
2020-02-27 11:28:29 -0500

    P2: Added the html and draggable droppable options in script for game 2
    - Added the html structure for game 2 display plus the css that goes with it
    - Also added in the script the draggable and droppable functions for the game Two

--- Projects/project02/js/script.js
+++ Projects/project02/js/script.js
@@ -73,13 +73,17 @@ $(document).ready(function() {
       },
 
       'go': function() {
-
+        //hide game one
         $('.game').hide();
-
+        //close the dialog box
         $('#dialog').dialog('close');
+        //show game two
+        $('.gameTwo').show();
+        //Hide clown image
+        $('.imageOne').hide();
 
         // Computer speach
-        responsiveVoice.speak("be careful, i would not play if i were you!", "UK English Male", {
+        responsiveVoice.speak("Are you scared yet?", "UK English Male", {
           rate: 0.2,
           pitch: 0.1,
           volume: 0.8
@@ -103,7 +107,7 @@ $(document).ready(function() {
 
 
 
-  //Setting up draggable elements for game1
+  //Setting up draggable elements for game One
 
   $(".drag").draggable({
     //contained in window
@@ -147,7 +151,9 @@ $(document).ready(function() {
             volume: 0.8
           });
         }, 3500);
+        // show the clown image
         $('.imageOne').show();
+        //Stop the game music
         gameSFX.loop = false;
         gameSFX.pause();
 
@@ -161,6 +167,14 @@ $(document).ready(function() {
             next: function() {
               $(this).dialog('close');
               $('.game').hide();
+              $('.gameTwo').show();
+              $('.imageOne').hide();
+              // Computer speach
+              responsiveVoice.speak("Are you scared yet?", "UK English Male", {
+                rate: 0.2,
+                pitch: 0.1,
+                volume: 0.8
+              });
 
             }
           }
@@ -169,5 +183,69 @@ $(document).ready(function() {
     }
 
   });
+
+  // End of game one
+
+
+  //Setting up draggable elements for game Two
+
+  $(".dragTwo").draggable({
+    //contained in window
+    containment: "window",
+    revert: 'invalid',
+
+  });
+
+  // Setting up droppable elements for game Two
+
+  $(".dropTwo").droppable({
+    //accept the draggables with same num attribute
+    accept: function(item) {
+      return $(this).attr('num') === item.attr('num');
+    },
+    //called when the appropriate draggable has been received
+    drop: function(event, ui) {
+
+      //disable draggable so u cant drag it after it's dropped in appropriate droppable
+      ui.draggable.draggable("disable");
+
+      //put the draggable in the center of droppable
+      ui.draggable.position({
+        my: "center",
+        at: "center",
+        of: $(this)
+      });
+
+      // keep track of how many draggables got droppedand add 1 each time
+      dropNum += 1;
+
+      //tracks the number of droppables and once it gets to 4
+      // show the dialog box
+      if (dropNum === 4) {
+
+        kidSFX.play();
+        setTimeout(function() {
+          responsiveVoice.speak("Since you started, now go to the next round", "UK English Male", {
+            rate: 0.2,
+            pitch: 0.1,
+            volume: 0.8
+          });
+        }, 3500);
+        $('.imageOne').show();
+        gameSFX.loop = false;
+        gameSFX.pause();
+
+
+      }
+    }
+
+  });
+
+  // End of game Two
+
+
+
+
+
 
 });
